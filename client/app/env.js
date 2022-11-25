3ea1c7efc6209f1507e1b625cc88efc46db0ae70
Gil <gilperez109@gmail.com>
2022-11-24 18:13:56 -0700

    first commit

---
+++ client/app/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
+export const domain = ''
+export const audience = 'https://dev-tyump4q1aeg3j427.us.auth0.com/api/v2/'
+export const clientId = 'xrQ4a6ndc7l7LLN5c6m21fsTbmO72Xsi'
